b5df8095a925af7aac0cb5df2a816806f2d86e37
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-08 16:13:26 +0300

    form top

---
+++ partials/topform.php
@@ -0,0 +1,24 @@
+<div class="top-form-wrap">
+	<div class="mbox">
+		<div class="top-form">
+			<div class="content-top-form cfix-after">
+				<h2>заказать консультацию</h2>
+				<p class="top-form-text">Быстро развивающаяся компания, работающая в таких направлениях, как Мерчендайзинг, Стаффинг и Аутстаффинг персонала</p>
+				<div class="form-top-wrap">
+					<form action="" method="POST" class="form-top">
+						<div class="input-wrap name">
+							<input type="text" name="name" placeholder="Ваше имя"/>
+						</div>
+						<div class="input-wrap phone">
+							<input type="text" name="phone" placeholder="Телефон"/>
+						</div>
+						<div class="input-wrap-submit">
+							<input type="submit" name="submit" value="Заказать" />
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
