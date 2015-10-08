25f94b1e75af98fe193b549422ae6b6cb9afc9ba
Dmitry <dmitry1940@mail.ru>
2015-10-08 16:41:32 +0300

    added font to body

--- index.php
+++ index.php
@@ -11,7 +11,43 @@
 		<section>
 			<div class="mbox">
 				<div class="title"><span>География</span></div>
-
+				<div class="map-img-wrap">
+					<div class="map-img-top">
+						<div class="map-img-inf">
+							<div class="map-img-inf-top-main">
+								<div class="map-img-inf-top-descript"></div>
+								<div class="map-img-value" data-value="475">0</div>
+								<div class="map-img-inf-top-descript">человек</div>
+							</div>
+							<div class="map-img-inf-bottom-main">
+								Количество персонала, не требующего профессиональной подготовки
+							</div>
+						</div>
+						<div class="map-img-inf">
+							<div class="map-img-inf-top-main">
+								<div class="map-img-inf-top-descript"></div>
+								<div class="map-img-value" data-value="125">0</div>
+								<div class="map-img-inf-top-descript">городов</div>
+							</div>
+							<div class="map-img-inf-bottom-main">
+								Количество персонала, требующего профессиональной подготовки
+							</div>
+						</div>
+						<div class="map-img-inf">
+							<div class="map-img-inf-top-main">
+								<div class="map-img-inf-top-descript">более</div>
+								<div class="map-img-value" data-value="20">0</div>
+								<div class="map-img-inf-top-descript">клиентов</div>
+							</div>
+							<div class="map-img-inf-bottom-main">
+								в крупнейших городах России
+							</div>
+						</div>
+					</div>
+					<div class="map-img-bottom">
+						<img src="images/map-img.png" alt="" />
+					</div>
+				</div>
 			</div>
 		</section>
 
@@ -62,7 +98,7 @@
 			<div class="mbox">
 				<div class="title"><span>отзывы</span></div>
 				<!-- artem calls -->
-				<? partial('artem') ?>
+				<? partial('artem'); ?>
 			</div>
 		</section>
 
