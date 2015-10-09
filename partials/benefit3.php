0202030f4ea4fde38130b37e8b2215131efd3faf
ArtGarm <artem.garmatyk@mail.ru>
2015-10-09 16:55:12 +0300

    fix

--- partials/benefit3.php
+++ partials/benefit3.php
@@ -5,7 +5,7 @@
             <img src="images/benefit3-img1.png " alt="">
             <div class="name"> Персонал </div>
             <div class="line"></div>
-            <div class="content"> Мы гарантируем тщательный подбор персонала, обучение и подго- товку к успешной работе. Нашими сотрудниками являются только граждане РФ.  </div>
+            <div class="content"> Мы гарантируем тщательный подбор персонала, обучение и подготовку к успешной работе. Нашими сотрудниками являются только граждане РФ.  </div>
         </div>
         <div class="benefit-item animate-it">
             <img src="images/benefit3-img2.png " alt="">
@@ -20,7 +20,7 @@
             <div class="content">Многоуровнеая система контроля полевого персонала через собственную эксклюзивную программу.</div>
         </div>
         <div class="benefit-item animate-it">
-            <img src="images/benefit3-img3.png " alt="">
+            <img src="images/benefit3-img4.png " alt="">
             <div class="name">Аналитика</div>
             <div class="line"></div>
             <div class="content">Предоставление аналитических отчетов  по результатам работы.</div>
