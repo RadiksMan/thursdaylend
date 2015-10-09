21d0c2a5caae36e6acd30ea9b58f607d7bc7a179
Radion <radion91@gmail.com>
2015-10-09 09:31:15 +0200

    fix

--- header.php
+++ header.php
@@ -54,20 +54,6 @@
 	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
 <!-- jScrollPane -->
 
-<!-- jandexMaps -->
-	<style>
-		.map1{width: 100%;height: 350px;}
-		.mapes{height: 350px;margin-bottom: 15px;}
-		.list{width: 200px;float: left;}
-		.list-item.hover{background-color: red;}
-		.checkboxes{float: right;margin-right: 10px;}
-		.checkboxes label{display: inline-block;margin-right: 10px;}
-		.clear{clear: both;}
-	</style>
-
-	<script src="http://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
-	<script src="js/jandexMapsScr.js" type="text/javascript"></script>
-<!-- jandexMaps -->
 
 <!-- remove Files -->
 	<!--<script src="js/myalert.js" type="text/javascript"></script>
