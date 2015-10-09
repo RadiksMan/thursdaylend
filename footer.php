2c7577599369bfc48ef9ee413bb92d33d83e2c5d
Dmitry <dmitry1940@mail.ru>
2015-10-09 12:23:13 +0300

    footer added

--- footer.php
+++ footer.php
@@ -1,8 +1,51 @@
 	<div class="footer_placeholder"></div>
-	<footer class="footer">
-		<div class="mbox">
-			
-		</div>
+	<footer class="footer styled-footer animate-section <?php  echo $act;?>">
+        <div class="footer-wrap">
+    		<div class="mbox">
+    			<div class="footer-left-column animate-it">
+                    <div class="footer-row cfix-after">
+                        <div class="footer-icon">
+                            <img src="images/house-icon.png" alt="" />
+                        </div>
+                        <div class="footer-text">
+                            <p>г. Москва, Проспект Мира д. 150</p>
+                        </div>
+                    </div>
+                    <div class="footer-row cfix-after">
+                        <div class="footer-icon">
+                            <img src="images/mail-icon.png" alt="" />
+                        </div>
+                        <div class="footer-text">
+                            <p>
+                                <a href="mailto:info@matrix-btl.ru">info@matrix-btl.ru</a>
+                            </p>
+                            <p>
+                                <a href="mailto:boxbox@google.com">boxbox@google.com</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-logo animate-it">
+                    <img src="images/footer-logo.png" alt="" />
+                </div>
+                <div class="footer-right-column animate-it">
+                    <div class="footer-row cfix-after">
+                        <div class="footer-icon">
+                            <img src="images/phone-icon.png" alt="" />
+                        </div>
+                        <div class="footer-text">
+                            <p>
+                                <a href="tell:+74953696082">+7 (495) 369 60 82</a>
+                            </p>
+                            <p>
+                                <a href="tell:+79181253214">+7 (918) 125 32 14</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="footer-row descript">Golden Marrow 2015</div>
+                </div>
+    		</div>
+        </div>
 	</footer>
 </body>
 </html>
