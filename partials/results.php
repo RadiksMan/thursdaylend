0c3df9345dfea06f92db2124510206d54b76d079
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-09 12:32:17 +0300

    validation fix

---
+++ partials/results.php
@@ -0,0 +1,40 @@
+<div class="results-wrap">
+	<div class="results">
+		<div class="result">
+			<div class="result-title">
+				<p>Результат №1</p>
+			</div>
+			<div class="result-description">
+				<p>Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.</p>
+			</div>
+			<img src="images/resultimg.jpg" alt=""/>
+		</div>
+		<div class="result">
+			<div class="result-title">
+				<p>Результат №1</p>
+			</div>
+			<div class="result-description">
+				<p>Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.</p>
+			</div>
+			<img src="images/resultimg.jpg" alt=""/>
+		</div>
+		<div class="result">
+			<div class="result-title">
+				<p>Результат №1</p>
+			</div>
+			<div class="result-description">
+				<p>Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.</p>
+			</div>
+			<img src="images/resultimg.jpg" alt=""/>
+		</div>
+		<div class="result">
+			<div class="result-title">
+				<p>Результат №1</p>
+			</div>
+			<div class="result-description">
+				<p>Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.</p>
+			</div>
+			<img src="images/resultimg.jpg" alt=""/>
+		</div>
+	</div>
+</div>
