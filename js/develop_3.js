7fcc0abacc0302edc36671969f8ef068716810eb
Radion <radion91@gmail.com>
2015-10-09 15:27:04 +0200

    no message

--- js/develop_3.js
+++ js/develop_3.js
@@ -131,15 +131,18 @@ $(document).ready(function() {
         wrapCSS:"call-popup",
 	    closeBtn:false
 	});
-   
+
 
     $('.results').slick({
-      centerMode: true,
-      centerPadding: '60px',
-      slidesToShow: 3
+       centerMode: true,
+      centerPadding: '0px',
+      slidesToShow: 3,
+      infinite: true,
+      draggable:false,
+      focusOnSelect:true
     });
 });
 
 
-	
+
 
