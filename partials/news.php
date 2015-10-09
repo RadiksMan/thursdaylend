2b6a5852016c8734ffd56378dd8bab9e30dcd9ec
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-09 11:18:24 +0300

    about ready

---
+++ partials/news.php
@@ -0,0 +1,48 @@
+<div class="newses-wrap">
+	<div class="mbox">
+		<div class="newses cfix-after">
+			<div class="news-wrap">
+				<div class="news">
+					<div class="news-img">
+						<img src="images/newsimg.jpg" alt=""/>
+					</div>
+					<div class="news-date-title">
+						<p><span>25/09</span> Бизнес и финансы</p>
+					</div>
+					<div class="news-description">
+						<p>Чаще помнят плохое, и найти для них уроки безукоризненными резюме научитесь. Хотите быть предпринимателем и конечно же, ни одной стороны, это. Проект, чтобы ваша жизнь ничему бы нас настоящими героями.</p>
+					</div>
+					<a href="#" class="news-readmore">Смотреть полностью <span></span></a>
+				</div>
+			</div>
+			<div class="news-wrap">
+				<div class="news">
+					<div class="news-img">
+						<img src="images/newsimg.jpg" alt=""/>
+					</div>
+					<div class="news-date-title">
+						<p><span>25/09</span> Бизнес и финансы</p>
+					</div>
+					<div class="news-description">
+						<p>Чаще помнят плохое, и найти для них уроки безукоризненными резюме научитесь. Хотите быть предпринимателем и конечно же, ни одной стороны, это. Проект, чтобы ваша жизнь ничему бы нас настоящими героями.</p>
+					</div>
+					<a href="#" class="news-readmore">Смотреть полностью <span></span></a>
+				</div>
+			</div>
+			<div class="news-wrap">
+				<div class="news">
+					<div class="news-img">
+						<img src="images/newsimg.jpg" alt=""/>
+					</div>
+					<div class="news-date-title">
+						<p><span>25/09</span> Бизнес и финансы</p>
+					</div>
+					<div class="news-description">
+						<p>Чаще помнят плохое, и найти для них уроки безукоризненными резюме научитесь. Хотите быть предпринимателем и конечно же, ни одной стороны, это. Проект, чтобы ваша жизнь ничему бы нас настоящими героями.</p>
+					</div>
+					<a href="#" class="news-readmore">Смотреть полностью <span></span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
